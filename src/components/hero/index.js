e268f9802953694e5ba43291ed32a1b894e8dbb1
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2020-03-31 23:29:46 -0400

    make logo go to episodes

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Logo from '../../images/toplogo.png'
 import Styles from './index.module.css';
 import SocialNav from './socialNav';
@@ -8,7 +9,9 @@ import SocialNavStyles from './socialNav.module.css'
 const Hero = () => (
   <>
     <div className={Styles.headerWrapper}>
-      <img className={Styles.heroImage} src={Logo} alt="Hero Image Banner" />
+      <Link to="/">
+        <img className={Styles.heroImage} src={Logo} alt="Hero Image Banner" />
+      </Link>
     </div>
     <SocialNav Styles={SocialNavStyles} />
     <NavBar />
